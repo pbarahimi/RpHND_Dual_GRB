5978d855cdcc0aef5d788287713e438e19d0272d
pbarahimi <pbarahimi@gmail.com>
2015-09-20 13:50:45 -0700

    092015 - implementing the dual

--- RpHND_Dual_GRB/src/Main.java
+++ RpHND_Dual_GRB/src/Main.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.security.InvalidAlgorithmParameterException;
+import java.util.List;
 
 import gurobi.GRB;
 import gurobi.GRBEnv;
@@ -17,7 +19,7 @@ public class Main {
 	private static double[][] distances = Distance.get(coordinates);
 	private static int nVar = tmpFlows.length;
 	private static double[][] flows = new double[nVar][nVar];
-	private static int D = 1; // Maximum number of simultaneous disruptions
+	private static int D = 2; // Maximum number of simultaneous disruptions
 	private static int R = (int) Math.pow(2, D + 1) - 2; // Largest index in the
 															// full binary tree.
 	private static int _R = (int) Math.pow(2, D) - 2; // Total number of nodes
@@ -72,20 +74,28 @@ public class Main {
 			return q;
 	}
 
-	public static void main(String[] args) throws FileNotFoundException {
+	/*public static void main(String[] args) throws InvalidAlgorithmParameterException{
+		
+		int i = 53921;
+		if (i%2 == 0)
+			System.out.println("Even");
+		else
+			System.out.println("odd");
+	}*/
+	public static void main(String[] args) throws FileNotFoundException, InvalidAlgorithmParameterException {
 		// Filling in the flows matrix assymetrically
 		for (int i = 0; i < nVar; i++) {
 			for (int j = 0; j < nVar; j++) {
 				flows[i][j] = tmpFlows[i][j] + tmpFlows[j][i];
 			}
 		}
-		System.out.println(_R);
+		
 		try {
 			GRBEnv env = new GRBEnv("RpHND_Dual.log");
 			GRBModel model = new GRBModel(env);
 
 			// Create variables
-			GRBVar u2 = model.addVar(0.0, GRB.INFINITY, P, GRB.CONTINUOUS, "u2");
+			GRBVar u2 = model.addVar(-1*GRB.INFINITY, GRB.INFINITY, P, GRB.CONTINUOUS, "u2");
 			GRBVar[][] u3 = new GRBVar[nVar][nVar];
 			GRBVar[][][][] u4 = new GRBVar[nVar][nVar][nVar][R + 1];
 			GRBVar[][][][] u5 = new GRBVar[nVar][nVar][nVar][R + 1];
@@ -100,7 +110,7 @@ public class Main {
 			for (int i = 0; i < nVar; i++) {
 				for (int j = i + 1; j < nVar; j++) {
 					String name = "u3_" + i + "_" + j;
-					u3[i][j] = model.addVar(0, GRB.INFINITY, 1, GRB.CONTINUOUS, name);
+					u3[i][j] = model.addVar(-1*GRB.INFINITY, GRB.INFINITY, 1, GRB.CONTINUOUS, name);
 				}
 			}
 
@@ -110,7 +120,7 @@ public class Main {
 					for (int k = 0; k < nVar; k++) {
 						for (int r = 0; r <= R; r++) {
 							String name = "u4_" + i + "_" + j + "_" + k + "_" + r;
-							u4[i][j][k][r] = model.addVar(0, GRB.INFINITY, 0, GRB.CONTINUOUS, name);
+							u4[i][j][k][r] = model.addVar(-1*GRB.INFINITY, 0, 0, GRB.CONTINUOUS, name);
 						}
 					}
 				}
@@ -122,7 +132,7 @@ public class Main {
 					for (int m = 0; m < nVar; m++) {
 						for (int r = 0; r <= R; r++) {
 							String name = "u5_" + i + "_" + j + "_" + m + "_" + r;
-							u5[i][j][m][r] = model.addVar(0, GRB.INFINITY, 0, GRB.CONTINUOUS, name);
+							u5[i][j][m][r] = model.addVar(-1*GRB.INFINITY, 0, 0, GRB.CONTINUOUS, name);
 						}
 					}
 				}
@@ -133,7 +143,7 @@ public class Main {
 				for (int j = i + 1; j < nVar; j++) {
 					for (int r = 0; r <= R; r++) {
 						String name = "u6_" + i + "_" + j + "_" + r;
-						u6[i][j][r] = model.addVar(0, GRB.INFINITY, M, GRB.CONTINUOUS, name);
+						u6[i][j][r] = model.addVar(-1*GRB.INFINITY, 0, M, GRB.CONTINUOUS, name);
 					}
 				}
 			}
@@ -143,7 +153,7 @@ public class Main {
 				for (int j = i + 1; j < nVar; j++) {
 					for (int r = 0; r <= R; r++) {
 						String name = "u7_" + i + "_" + j + "_" + r;
-						u7[i][j][r] = model.addVar(0, GRB.INFINITY, M, GRB.CONTINUOUS, name);
+						u7[i][j][r] = model.addVar(-1*GRB.INFINITY, 0, M, GRB.CONTINUOUS, name);
 					}
 				}
 			}
@@ -153,7 +163,7 @@ public class Main {
 				for (int j = i + 1; j < nVar; j++) {
 					for (int r = 0; r <= _R; r++) { //Leaf-nodes excluded
 						String name = "u8_" + i + "_" + j + "_" + r;
-						u8[i][j][r] = model.addVar(0, GRB.INFINITY, 0, GRB.CONTINUOUS, name);
+						u8[i][j][r] = model.addVar(-1*GRB.INFINITY, 0, 0, GRB.CONTINUOUS, name);
 					}
 				}
 			}
@@ -163,7 +173,7 @@ public class Main {
 				for (int j = i + 1; j < nVar; j++) {
 					for (int r = 0; r <= _R; r++) {	//Leaf-nodes excluded
 						String name = "u9_" + i + "_" + j + "_" + r;
-						u9[i][j][r] = model.addVar(0, GRB.INFINITY, 0, GRB.CONTINUOUS, name);
+						u9[i][j][r] = model.addVar(-1*GRB.INFINITY, 0, 0, GRB.CONTINUOUS, name);
 					}
 				}
 			}
@@ -174,7 +184,7 @@ public class Main {
 					for (int k = 0; k < nVar; k++) {
 						for (int r = 0; r <= _R; r++) { //Leaf-nodes excluded
 							String name = "u10_" + i + "_" + j + "_" + k + "_" + r;
-							u10[i][j][k][r] = model.addVar(0, GRB.INFINITY, M, GRB.CONTINUOUS, name);
+							u10[i][j][k][r] = model.addVar(-1*GRB.INFINITY, 0, M, GRB.CONTINUOUS, name);
 						}
 					}
 				}
@@ -186,7 +196,7 @@ public class Main {
 					for (int m = 0; m < nVar; m++) {
 						for (int r = 0; r <= _R; r++) { //Leaf-nodes excluded
 							String name = "u11_" + i + "_" + j + "_" + m + "_" + r;
-							u11[i][j][m][r] = model.addVar(0, GRB.INFINITY, M, GRB.CONTINUOUS, name);
+							u11[i][j][m][r] = model.addVar(-1*GRB.INFINITY, 0, M, GRB.CONTINUOUS, name);
 						}
 					}
 				}
@@ -287,36 +297,34 @@ public class Main {
 				for (int j = i + 1; j < nVar; j++) {
 					for (int k=0; k<nVar; k++){
 						for (int m = 0; m < nVar; m++) {
-							for (int r=0; r<=R; r++){
+							for (int r=1; r<=_R; r++){  // only non-leaf nodes are included in these constraints
 								GRBLinExpr con3 = new GRBLinExpr();
 								con3.addTerm(1, u4[i][j][k][r]);
 								con3.addTerm(1, u5[i][j][k][r]);
 								if (i!=k && i!=m)
 									con3.addTerm(1, u6[i][j][r]);
 								if (j!=k && j!=m)
-									con3.addTerm(1, u7[i][j][r]);
-								
-								if (r<=_R){	// if 'r' is not a leaf-node
-									if (k!=i && k!=j)
-										con3.addTerm(1, u8[i][j][r]);
+									con3.addTerm(1, u7[i][j][r]);								
 									
-									if (m!=i && m!=j)
-										con3.addTerm(1, u9[i][j][r]);
-									if (r%2 == 0)
-										con3.addTerm(-1, u9[i][j][(int) Math.floor((r-1)/2)]);
+								if (k!=i && k!=j)
+									con3.addTerm(1, u8[i][j][r]);
+								/////////////
+								if (m!=i && m!=j)
+									con3.addTerm(1, u9[i][j][r]);
+								if (r%2 == 0)
+									con3.addTerm(-1, u9[i][j][(int) Math.floor((r-1)/2)]);
+								else
+									con3.addTerm(-1, u8[i][j][(int) Math.floor((r-1)/2)]);										
+								con3.addTerm(M, u10[i][j][k][r]);
+								con3.addTerm(M, u11[i][j][m][r]);
+								
+								for (int parent : BinaryTree.getParents(r)){
+									if (BinaryTree.isLeftChild(parent, r))
+										con3.addTerm(1, u10[i][j][k][parent]);
 									else
-										con3.addTerm(-1, u8[i][j][(int) Math.floor((r-1)/2)]);										
-									con3.addTerm(M, u10[i][j][k][r]);
-									for (int s : BinaryTree.leftChildren(r, D)){
-										con3.addTerm(1, u10[i][j][k][s]);
-										con3.addTerm(1, u10[i][j][m][s]);
-									}
-									con3.addTerm(M, u11[i][j][m][r]);
-									for (int s : BinaryTree.rightChildren(r, D)){
-										con3.addTerm(1, u11[i][j][k][s]);
-										con3.addTerm(1, u11[i][j][m][s]);
-									}
+										con3.addTerm(1, u11[i][j][m][parent]);
 								}
+																
 								System.out.println();
 								double CoEf = flows[i][j] * Cikmj(i, k, m, j) * Math.pow(q, Math.floor(Math.log(r+1)/Math.log(2)));
 								model.addConstr(con3, GRB.LESS_EQUAL, CoEf, "c3" + i + "_" + j + "_" + k + "_" + m + "_" + r);
@@ -327,43 +335,66 @@ public class Main {
 			}
 			
 			// Constraint 4
+			for (int i = 0; i < nVar; i++) {
+				for (int j = i + 1; j < nVar; j++) {
+					for (int k=0; k<nVar; k++){
+						for (int m = 0; m < nVar; m++) {
+							for (int r=_R+1; r<=R; r++){  // only leaf nodes are included in these constraints
+								GRBLinExpr con4 = new GRBLinExpr();
+								con4.addTerm(1, u4[i][j][k][r]);
+								con4.addTerm(1, u5[i][j][k][r]);
+								if (i!=k && i!=m)
+									con4.addTerm(1, u6[i][j][r]);
+								if (j!=k && j!=m)
+									con4.addTerm(1, u7[i][j][r]);								
+								double CoEf = flows[i][j] * Cikmj(i, k, m, j) * Math.pow(q, Math.floor(Math.log(r+1)/Math.log(2)));
+								model.addConstr(con4, GRB.LESS_EQUAL, CoEf, "c3" + i + "_" + j + "_" + k + "_" + m + "_" + r);
+							}							
+						}
+					}
+				}
+			}
 			
+			// Constraint 5
 			for (int x=0; x<nVar; x++){
-				GRBLinExpr con4 = new GRBLinExpr();
-				con4.addTerm(1, u2);
+				GRBLinExpr con5 = new GRBLinExpr();
+				con5.addTerm(1, u2);
 				for (int i = 0; i < nVar; i++) {
 					for (int j = i + 1; j < nVar; j++) {
 						for (int r = 0; r <= R; r++) {
-							con4.addTerm(-1, u4[i][j][x][r]);
+							con5.addTerm(-1, u4[i][j][x][r]);
 						}
 					}
 				}
 				for (int i = 0; i < nVar; i++) {
 					for (int j = i + 1; j < nVar; j++) {
 						for (int r = 0; r <= R; r++) {
-							con4.addTerm(-1, u5[i][j][x][r]);
+							con5.addTerm(-1, u5[i][j][x][r]);
 						}
 					}
 				}
 				for (int j=x+1; j<nVar; j++){
 					for (int r = 0; r <= R; r++) {
-						con4.addTerm(M, u6[x][j][r]);
+						con5.addTerm(M, u6[x][j][r]);
 					}
 				}
 				for (int i=0; i<nVar; i++){
 					for (int r = 0; r <= R; r++) {
 						if (x>i)
-							con4.addTerm(M, u7[i][x][r]);
+							con5.addTerm(M, u7[i][x][r]);
 					}
 				}
-				model.addConstr(con4, GRB.LESS_EQUAL, 0, "con4" + x);
+				model.addConstr(con5, GRB.LESS_EQUAL, 0, "con4" + x);
 			}
 			
 			// Optimize model
 			model.optimize();
 			
+			System.out.println("Number of varibles: " + model.get(GRB.IntAttr.NumVars));
+			System.out.println("Number of constraints: " + model.get(GRB.IntAttr.NumConstrs));
+						
 			//Printing solution to a file
-			File file = new File("D:/Dual_results.txt");
+			/*File file = new File("D:/Dual_results.txt");
 			PrintWriter out = new PrintWriter(file);
 			
 			// u3_i,j
@@ -460,15 +491,16 @@ public class Main {
 						}
 					}
 				}
-			}
+			}*/
+			
 			
 			//Results 
-			System.out.println("Obj: " + model.get(GRB.DoubleAttr.ObjVal));
-			out.println("Obj: " + model.get(GRB.DoubleAttr.ObjVal));
+//			System.out.println("Obj: " + model.get(GRB.DoubleAttr.ObjVal));
+//			out.println("Obj: " + model.get(GRB.DoubleAttr.ObjVal));
 			// Dispose of model and environment
 		    model.dispose();
 		    env.dispose();
-			out.close();
+//			out.close();
 		} catch (GRBException e) {
 			System.out.println("Error code: " + e.getErrorCode() + ". " + e.getMessage());
 		}
